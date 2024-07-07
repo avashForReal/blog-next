28cb18475a6e66eabfe5c44e34263d4cd9480c3b
avash <avash.ghimire@rumsan.net>
2024-07-07 16:06:19 +0545

    added projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,21 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Rahat Anticipatory Action',
+    description: `Rahat Anticipatory Action is a project under the Rahat platform, an open-source, blockchain-based financial access platform designed to streamline humanitarian aid distribution. It distributes funds by predicting floods and sends automated early warnings in the form of SMS and IVR to the vulnerable communities of the flood prone area. Using blockchain, it ensures secure and transparent transactions, enabling efficient aid distribution through digital tokens.`,
+    imgSrc: '/static/images/projects/rahat.jpg',
+    href: 'https://github.com/rahataid',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Foodony',
+    description: `Foodony is an online food delivery platform designed to cater to the local demand for convenient and diverse meal options. This platform provides a seamless experience for ordering food, integrating a reliable payment system, real-time tracking of placed orders etc. Foodony ensures a hassle-free and enjoyable food ordering experience, connecting customers with their favorite local restaurants effortlessly.`,
+    imgSrc: '/static/images/projects/foodony.jpg',
+    href: '/blog/foodony',
+  },
+  {
+    title: 'Foodony Vendors',
+    description: `Foodony Vendors is a platform designed for restaurants to work in tandem with the Foodony user application. It provides restaurants with instant order notifications and robust menu management tools, allowing them to easily update their offerings and highlight specials or promotions. Additionally, Foodony Vendors offers comprehensive reporting features, enabling restaurants to track sales performance and access customer feedback. This platform empowers restaurants to optimize their operations, enhance customer satisfaction, and drive business growth.`,
+    imgSrc: '/static/images/projects/foodony.jpg',
+    href: '/blog/foodony',
   },
 ]
 
